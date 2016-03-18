e348d0a899a9cc46479050af631a484f1edb49f8
amenema <men_zhong_xin@163.com>
2016-03-18 17:12:27 +0800

    fix bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -12,8 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index.html');
 });
-router.get('/test',function(req,res){
-  res.end({});
-});
+
 
 
 module.exports = router;
