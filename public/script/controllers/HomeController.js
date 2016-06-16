31702a7dfbf2b27ca26fd0629592baf92afe2f82
amenema <men_zhong_xin@163.com>
2016-06-16 20:54:05 +0800

    remove html

--- public/script/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by menzhongxin on 16/3/29.
- */
-(function(){
-  angular.module('secApp.HomeController',[])
-    .controller('HomeController',function($http,$scope){
-
-    });
-})();
--- public/script/controllers/LoginController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by menzhongxin on 16/3/29.
- */
-(function(){
-  angular.module('secApp.LoginController',[])
-    .controller('LoginController',function($http,$scope){
-
-    });
-})();
